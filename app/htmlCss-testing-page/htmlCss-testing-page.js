cd4d046d6101080fe2a7326f844d9e3d6953b288
AndreWilliams7 <70011803+AndreWilliams7@users.noreply.github.com>
2022-10-06 13:37:44 +0200

    Grid Testing
    Added grid build function to test

--- app/htmlCss-testing-page/htmlCss-testing-page.js
+++ app/htmlCss-testing-page/htmlCss-testing-page.js
@@ -3,6 +3,11 @@ export default class HtmlCssTesting extends crsbinding.classes.ViewBase {
         // this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
         await super.connectedCallback();
         console.log("Testing");
+        this.gridWindow = document.getElementById("grid-window");
+
+        requestAnimationFrame(() => {
+            this.drawGrid(3, 8);
+        });
     }
 
     disconnectedCallback() {
@@ -12,4 +17,17 @@ export default class HtmlCssTesting extends crsbinding.classes.ViewBase {
     async test() {
         console.log("Testing");
     }
+
+    async drawGrid(rows, cols) {
+        this.gridWindow.style.setProperty("--grid-rows", rows);
+        this.gridWindow.style.setProperty("--grid-cols", cols);
+
+        for(let i = 0; i < rows*cols; i++){
+            let cell = document.createElement("div");
+            cell.innerText = (i +1);
+            this.gridWindow.appendChild(cell).className = "grid-item";
+        }
+    }
+
+    
 }
